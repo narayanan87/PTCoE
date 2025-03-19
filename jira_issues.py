cd3e1dd730162188e81959d8e4db04bd3039a182
narayanan87 <narayanan.azhagappan@kone.com>
2025-03-19 14:29:53 +0530

    Create jira_issues.py

---
+++ jira_issues.py
@@ -0,0 +1,34 @@
+import csv
+from jira import JIRA
+
+jira_server = "https://kone.atlassian.net"
+jira_user = "narayanan.azhagappan@kone.com"
+jira_pass = ${{secretes.pass}}
+
+jira_connect = JIRA(server=jira_server, basic_auth=(jira_user, jira_pass))
+
+jira_projects = jira_connect.projects()
+print("List of All Projects")
+
+issues = []
+
+try:
+    for project in jira_projects:
+        issues += jira_connect.search_issues(f"project = \"{project.key}\" AND issuetype IN (Epic, Story, Bug, Task) AND sprint IN opensprints()")
+    print("Connection Successful")
+except Exception as e:
+    print(f"Error connecting: {str(e)}")
+
+# Fields to extract
+jirafields = ["project", "team", "sprint", "issuetype", "summary", "issuekey", "created", "updated", "assignee", "email", "tester", "status"]
+
+with open('C:/Users/k64152761/OneDrive - KONE Corporation/Documents/QADashboard/extracted_issues.csv', mode='w', newline='') as csv_file:
+    writer = csv.DictWriter(csv_file, fieldnames=jirafields)
+    writer.writeheader()
+
+    for issue in issues:
+        issue_detail = jira_connect.issue(issue.key)
+        issue_data = {field: getattr(issue_detail.fields, field, '') for field in jirafields}
+        writer.writerow(issue_data)
+
+print("Data written successfully to extracted_issues.csv")
