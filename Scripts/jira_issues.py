41e70eedd594fefb9d21d10b9f35b1fcaf2dbfc6
narayanan87 <narayanan.azhagappan@kone.com>
2025-03-19 15:48:37 +0530

    Update jira_issues.py

--- Scripts/jira_issues.py
+++ Scripts/jira_issues.py
@@ -10,28 +10,26 @@ jira_connect = JIRA(server=jira_server, basic_auth=(jira_user, jira_pass))
 
 jira_projects = ["CHSS", "MODS", "NSS", "MATE", "CSCAI", "COTC"]
 
-for project in jira_projects:
-    print(project.key)
+issues = []
 
-    issues = []
-    
-    try:
-        for project in jira_projects:
-            issues += jira_connect.search_issues(f"project = \"project\" AND issuetype IN (Epic, Story, Bug, Task) AND sprint IN opensprints()")
-        print("Connection Successful")
-    except Exception as e:
-        print(f"Error connecting: {str(e)}")
-    
-    # Fields to extract
-    jirafields = ["project", "team", "sprint", "issuetype", "summary", "issuekey", "created", "updated", "assignee", "email", "tester", "status"]
-    
-    with open('C:/Users/k64152761/OneDrive - KONE Corporation/Documents/QADashboard/extracted_issues.csv', mode='w', newline='') as csv_file:
-        writer = csv.DictWriter(csv_file, fieldnames=jirafields)
-        writer.writeheader()
-    
-        for issue in issues:
-            issue_detail = jira_connect.issue(issue.key)
-            issue_data = {field: getattr(issue_detail.fields, field, '') for field in jirafields}
-            writer.writerow(issue_data)
-    
+try:
+    for project in jira_projects:
+        print(f"Processing project: {project}")
+        issues += jira_connect.search_issues(f'project = "{project}" AND issuetype IN (Epic, Story, Bug, Task) AND sprint IN opensprints()')
+    print("Connection Successful")
+except Exception as e:
+    print(f"Error connecting: {str(e)}")
+
+# Fields to extract
+jirafields = ["project", "team", "sprint", "issuetype", "summary", "issuekey", "created", "updated", "assignee", "email", "tester", "status"]
+
+with open('C:/Users/k64152761/OneDrive - KONE Corporation/Documents/QADashboard/extracted_issues.csv', mode='w', newline='') as csv_file:
+    writer = csv.DictWriter(csv_file, fieldnames=jirafields)
+    writer.writeheader()
+
+    for issue in issues:
+        issue_detail = jira_connect.issue(issue.key)
+        issue_data = {field: getattr(issue_detail.fields, field, '') for field in jirafields}
+        writer.writerow(issue_data)
+
 print("Data written successfully to extracted_issues.csv")
